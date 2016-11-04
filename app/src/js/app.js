1944e63c484a992836e54cd5f591ba4525b33e17
martin bouchard <marbouchl@gmail.com>
2016-11-04 09:48:35 -0400

    hello

---
+++ app/src/js/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render(){
+        return (
+            <h1>dsfsdf</h1>
+        );
+    }
+}
+const app  = document.getElementById("app");
+ReactDOM.render(<Layout/>, app);
