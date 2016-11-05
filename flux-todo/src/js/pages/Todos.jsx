79569830d6415afddb8921c8b70682d3ca0330f4
martin bouchard <marbouchl@gmail.com>
2016-11-05 19:04:52 -0400

    todostore first step

--- flux-todo/src/js/pages/Todos.jsx
+++ flux-todo/src/js/pages/Todos.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import TodoStore from "../stores/TodoStore";
 
 export default class Todos extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: TodoStore.getAll()
+        }
+
+    }
     render() {
+        const { todos } = this.state;
+        const TodoComponents = todos.map((todo) => {
+            return <TodoItem key={todo.id} {...todo} />;
+        });
+
         return (
             <div>
                 <h1>Todos</h1>
+                <ul>
+                    {TodoComponents}
+                </ul>
             </div>
         );
+    }
+}
+
+export class TodoItem extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+
+        const { complete, edit, text } = this.props;
+        const icon = complete ? "\u2714" : "\u2716"
+
+        return (
+            <li>
+                <span style={{ marginRight: "10px" }}>{text}</span>
+                <span>{icon}</span>
+            </li>
+        )
     }
 }
