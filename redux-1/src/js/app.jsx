a9480dffb9a7faa67551d535197c836f701aa2cb
martin bouchard <marbouchl@gmail.com>
2016-11-06 11:44:22 -0500

    checkpoint

---
+++ redux-1/src/js/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+const app = document.getElementById("app");
+ReactDom.render(
+    <div>
+        <h1>ALLO</h1>
+    </div>
+    , app
+);
--- redux-1/webpack.config.js
+++ redux-1/webpack.config.js
@@ -5,7 +5,7 @@ var path = require("path");
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : null,
-    entry: ".js/app.jsx",
+    entry: "./js/app.jsx",
     module: {
         loaders: [{
             text: /\.jsx?$/,
