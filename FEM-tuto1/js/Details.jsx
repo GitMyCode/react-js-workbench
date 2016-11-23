51dbf1b31af540155b4e3ed60e53fd9e73d00a81
martin bouchard <marbouchl@gmail.com>
2016-11-23 11:40:09 -0500

    passer le film en parametre a la page details

---
+++ FEM-tuto1/js/Details.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+
+class Details extends React.Component {
+  render () {
+    return (
+      <div className="container" style={{textAlign: 'left'}}>
+        <pre><code>{JSON.stringify(this.props.params, null, 4)}</code></pre>
+      </div>
+        )
+  }
+}
+
+const { object } = React.PropTypes
+
+Details.propTypes = {
+  params: object.isRequired
+}
+
+module.exports = Details
